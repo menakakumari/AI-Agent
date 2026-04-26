5ef9aee1b87095ff4bf11600e4f1adc4bb7172a7
Menaka <menakakumariofficial@gmail.com>
2026-04-26 14:28:05 +0530

    Update connection.js

--- src/database/connection.js
+++ src/database/connection.js
@@ -2,9 +2,10 @@ import path from "path";
 import { Kysely, SqliteDialect } from "kysely";
 import Database from "better-sqlite3";
 
-const { APP_PATH } = process.env;
+// const { APP_PATH } = process.env;
 
-const sqlite = new Database(path.join(APP_PATH, "sqlite/sqlite.db"), {
+// const sqlite = new Database(path.join(APP_PATH, "sqlite/sqlite.db"), {
+const sqlite = new Database("../../sqlite/sqlite.db"), {
   fileMustExist: false,
 });
 
